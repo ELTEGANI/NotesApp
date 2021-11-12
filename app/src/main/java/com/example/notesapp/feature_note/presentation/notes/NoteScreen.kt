7c874f5979cf4c91f11cd97d497cef87f75ee4e2
altegani mohammed <tiganimoh@gmail.com>
2021-11-12 14:36:32 +0200

    add notes screen

---
+++ app/src/main/java/com/example/notesapp/feature_note/presentation/notes/NoteScreen.kt
@@ -0,0 +1,99 @@
+package com.example.notesapp.feature_note.presentation.notes
+
+import androidx.compose.animation.*
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Sort
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavController
+import com.example.notesapp.feature_note.presentation.notes.components.NoteItem
+import com.example.notesapp.feature_note.presentation.notes.components.OrderSection
+
+
+@ExperimentalAnimationApi
+@Composable
+fun NoteScreen(
+    navController: NavController,
+    viewModel: NotesViewModel = hiltViewModel()
+){
+   val state = viewModel.state.value
+   val scaffoldState = rememberScaffoldState()
+   val scope = rememberCoroutineScope()
+
+    Scaffold (
+        floatingActionButton = {
+            FloatingActionButton(onClick = {
+
+            },
+            backgroundColor = MaterialTheme.colors.primary
+            ) {
+              Icon(imageVector = Icons.Default.Add, contentDescription = "Add Note")
+            }
+        },
+        scaffoldState = scaffoldState,
+            ){
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp)
+        ) {
+          Row(
+              modifier = Modifier.fillMaxSize(),
+              horizontalArrangement = Arrangement.SpaceBetween,
+              verticalAlignment = Alignment.CenterVertically
+          ) {
+             Text(
+                 text = "Your note",
+                 style = MaterialTheme.typography.h4
+             )
+              IconButton(onClick = {
+                 viewModel.onEvent(NotesEvent.ToggleOrderSection)
+              }) {
+                Icon(
+                    imageVector = Icons.Default.Sort,
+                    contentDescription = "Sort"
+                )
+              }
+          }
+        }
+        AnimatedVisibility(
+            visible = state.isOrderSectionVisible,
+            enter = fadeIn() + slideInVertically(),
+            exit = fadeOut() + slideOutVertically()
+        ) {
+            OrderSection(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 16.dp),
+                noteOrder = state.noteOrder,
+                onOrderChange = {
+                    viewModel.onEvent(NotesEvent.Order(it))
+                }
+            )
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+        LazyColumn(modifier = Modifier.fillMaxSize()){
+            items(state.notes){note->
+                NoteItem(
+                    note = note,
+                    modifier = Modifier.fillMaxSize().clickable{
+
+                    },
+                    onDeleteClick = {
+                        viewModel.onEvent(NotesEvent.DeleteNote(note))
+                    }
+                    )
+            }
+        }
+    }
+}
