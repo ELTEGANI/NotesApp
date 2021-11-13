4ff2abcc61bfd605a72a0150490d29e7fdaceeb2
altegani mohammed <tiganimoh@gmail.com>
2021-11-13 15:13:54 +0200

    add addeditnoteviewmodel

---
+++ app/src/main/java/com/example/notesapp/feature_note/domain/use_case/GetNote.kt
@@ -0,0 +1,12 @@
+package com.example.notesapp.feature_note.domain.use_case
+
+import com.example.notesapp.feature_note.domain.model.Note
+import com.example.notesapp.feature_note.domain.repository.NoteRepository
+
+class GetNote(
+    private val noteRepository: NoteRepository
+) {
+   suspend fun invoke(id:Int): Note?{
+    return noteRepository.getNoteById(id)
+   }
+}
