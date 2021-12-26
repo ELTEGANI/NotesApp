0141c43c7ce1328a96822973d0c2a6fcd478b7f8
ELTEGANI <tiganimoh@gmail.com>
2021-12-27 03:12:12 +0400

    add integeration test

--- app/src/androidTest/java/com/example/notesapp/feature_note/presentation/notes/NoteScreenTest.kt
+++ app/src/androidTest/java/com/example/notesapp/feature_note/presentation/notes/NoteScreenTest.kt
@@ -1,8 +1,14 @@
 package com.example.notesapp.feature_note.presentation.notes
 
+import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.notesapp.di.AppModule
 import com.example.notesapp.feature_note.presentation.MainActivity
+import com.example.notesapp.feature_note.presentation.util.Screen
+import com.example.notesapp.ui.theme.CleanArchitectureNoteAppTheme
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
 import dagger.hilt.android.testing.UninstallModules
@@ -20,8 +26,17 @@ class NoteScreenTest{
     @get:Rule(order = 1)
     val composeRule = createAndroidComposeRule<MainActivity>()
 
+    @ExperimentalAnimationApi
     @Before
     fun setUp(){
         hiltRule.inject()
+        composeRule.setContent {
+            val navController = rememberNavController()
+            CleanArchitectureNoteAppTheme{
+                NavHost(navController = navController, startDestination = Screen.NotesScreen.route){
+                    composable(route = Screen.NotesScreen.route) { NotesScreen(navController = navController) }
+                }
+            }
+        }
     }
 }
